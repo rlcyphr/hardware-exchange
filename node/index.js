f457cc289fdc8f78a6b0bbae00c51d4fc81c3b57
Connor Fallon <connor.fallon@centralfax.co.uk>
2018-08-06 18:23:22 +0100

    Web server added.

--- node/index.js
+++ node/index.js
@@ -1,3 +1,20 @@
 console.log("---------------------------------");
 console.log("Hardware exchange server started");
 console.log("---------------------------------");
+
+var express = require("express");
+var app = express();
+
+app.get("/componenttypes", (req, res) => {
+    
+    res.send("Hello World");
+    
+});
+
+
+app.listen(8080, () => {
+    
+    console.log("Server started");
+    
+    
+});
