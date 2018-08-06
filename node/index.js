062046ac8a36eec4a89799a9af51ab82685fe028
Connor Fallon <connor.fallon@centralfax.co.uk>
2018-08-06 18:57:17 +0100

    Created first API call GET /componenttypes.

--- node/index.js
+++ node/index.js
@@ -3,11 +3,42 @@ console.log("Hardware exchange server started");
 console.log("---------------------------------");
 
 var express = require("express");
+var pg = require("pg");
+const config = {
+    user: 'postgres',
+    database: 'hardware-exchange',
+    password: 'layman wizard trials',
+    port: 5432
+};
+var pool = new pg.Pool(config);
 var app = express();
 
 app.get("/componenttypes", (req, res) => {
     
-    res.send("Hello World");
+    var sql = 'select * from public."componentType";';
+    
+    // call database
+    
+    pool.connect((error, client, done) => {
+        
+        client.query(sql, (error, result) => {
+            
+            if (error) {
+                
+                console.log(error);
+            } else {
+                
+                client.end();
+                res.send(result);
+                done();
+            }
+            
+            
+        });    
+        
+        
+        
+    });
     
 });
 
