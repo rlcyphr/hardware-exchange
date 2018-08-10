9a58393d94a8c4ef20ab9a84ef0d8fa061c14ccc
Connor Fallon <connor.fallon@centralfax.co.uk>
2018-08-10 16:29:25 +0100

    Added handlebars rendering, converted website from IIS to express, added API endpoint for register (insert SQL)

--- node/index.js
+++ node/index.js
@@ -2,7 +2,11 @@ console.log("---------------------------------");
 console.log("Hardware exchange server started");
 console.log("---------------------------------");
 
+// server - request and render pages using handlebars and get/post data to db
+// uses port 8081
+
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
 var pg = require("pg");
 var body_parser = require("body-parser");
 const config = {
@@ -13,11 +17,25 @@ const config = {
 };
 
 var pool = new pg.Pool(config); // creating a connection
+const path = require('path');
+
 var app = express(); // start the web server
-app.use(body_parser.json());
-app.use(body_parser.urlencoded( {extended:true} )); // tell express server to use the body parser - lets it grab contents of posted form fields
+app.engine('handlebars', expressHandlebars()); // set express to use handlebars renderer
+app.set('view engine', 'handlebars'); // set view engine to handlebars
 
+app.set('views', path.join(process.cwd(), 'views')); // set base dir for sites to /node/views  
+app.use(body_parser.json()); // allow server to understand and display json on screen
+app.use(body_parser.urlencoded( {extended:true} )); // tell express server to use the body parser - 
+                                                     // lets it grab contents of posted form fields
+app.use('/', express.static(path.join(process.cwd(), 'public'))) // set static assets directory to /public for handlebars renderer
 
+// -------- website calls --------
+
+app.get('/', (req, res) => {res.render('index')} ); // get website page from webpage root (/views) and render them 
+app.get('/register', (req, res) => {res.render('register')} );
+app.get('/thanks', (req, res) => {res.render('thanks')} );
+
+// -------- api calls --------
 
 app.get("/componenttypes", (req, res) => { 
     // use web server to get and push to path /componenttypes
@@ -39,7 +57,55 @@ app.get("/componenttypes", (req, res) => {
             } else {
                 
                 client.end();
+                done();
                 res.send(result);
+                
+            }
+            
+            
+        });    
+        
+    });
+    
+});
+
+
+app.post("/register", (req, res) => {
+
+    // register new user and insert into table
+
+    var sql = '';
+    sql = sql + 'INSERT INTO public."user" ( ';
+    sql = sql + 'email, ';
+    sql = sql + '"passwordHash", ';
+    sql = sql + '"dateCreated", ';
+    sql = sql + '"username"';
+    sql = sql + ' ) ';
+    sql = sql + 'VALUES ($1, $2, $3, $4); ';
+
+    // instruction for the database to run
+    // get details from the form based on the names of the included fields 
+    
+    var email = req.body.email || '';
+    var passwordHash = req.body.passwd || '';
+    var dateCreated = Date.now();
+    var username = req.body.username || '';
+
+    // call database
+    
+    pool.connect((error, client, done) => { 
+        // login to the server with credentials
+
+        client.query(sql, [email, passwordHash, dateCreated, username], (error, result) => { 
+        // run sql insert command and provide details - these are returned from the form sent from the client
+            
+            if (error) {
+                
+                console.log(error);
+            } else {
+                
+                client.end();
+                res.redirect('/thanks');
                 done();
             }
             
@@ -49,45 +115,6 @@ app.get("/componenttypes", (req, res) => {
         
         
     });
-    
-});
-
-
-app.post("/register", (req, res) => { // use web server to get and push to path /componenttypes
-    
-    console.log("test,.....");
-    console.log(req.body);
-   // console.log();
-    res.sendStatus(200);
-
-    
-    var sql = 'select * from public."componentType";'; 
-    // instruction for the database to run
-    
-    // call database
-    
-    //pool.connect((error, client, done) => { 
-        // login to the server with credentials
-
-        /* client.query(sql, (error, result) => { 
-        // use logged in client to retrieve the results of the database
-            
-            if (error) {
-                
-                console.log(error);
-            } else {
-                
-                client.end();
-                res.send(result);
-                done();
-            }
-            
-            
-        });    
-        */
-        
-        
-    //});
     
 });
 
