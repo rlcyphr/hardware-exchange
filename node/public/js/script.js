f8b80e53022cba1b4ab6ac71ccc4ebc7bb441517
Connor Fallon <connor.fallon@centralfax.co.uk>
2018-08-12 20:03:32 +0100

    Finished fix for navbar on mobile/resizing issues

--- node/public/js/script.js
+++ node/public/js/script.js
@@ -1,26 +1,40 @@
-$(document).ready(() => {
+// script to control navbar on mobile and when resizing the screen
 
-    $('ul li a').addClass('hidden');
-    
-    $('.hamburger-menu').click(() => {
+$(document).ready(function() {
+
+    $('.hamburger-menu').click(function() {
         
 
         if ($('ul li a').hasClass('hidden') ) {
 
             $('ul li a').removeClass('hidden');
+            $('.main-nav').addClass('nav-active');
 
         } else {
 
             $('ul li a').addClass('hidden');
-
+            $('.main-nav').removeClass('nav-active');
         }
         
     });
 
 
-    $(window).resize(() => {
+    $(window).resize(function() {
+
+        $('ul li a').addClass('hidden');
+
+        if ($(document).width() > 768) {
+            // if larger than 768 use the larger size layout 
+            $('.main-nav').removeClass('nav-active');
+            $('ul li a').removeClass('hidden');
+
+        } else {
+            // else use the mobile layout
+            $('ul li a').addClass('hidden');
+            $('.main-nav').removeClass('nav-active');
+        }
         
-        $('ul li a').css("display","none");
+        
 
     });
  
